57c657231295c1f9c0112906f69e7515514aec7a
Aadarsh <aadarshkumar8871860855@gmail.com>
2023-12-16 17:05:40 +0530

    changes in next-config and prisma .schema file

--- next.config.js
+++ next.config.js
@@ -18,21 +18,6 @@ const nextConfig = {
                 pathname: '**'
             },
         ]
-    },
-    env: {
-        DATABASE_URL: process.env.DATABASE_URL,
-        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-        GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-        GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-        NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-        NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET: process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET,
-        PUSHER_APP_ID: process.env.PUSHER_APP_ID,
-        NEXT_PUBLIC_PUSHER_KEY: process.env.NEXT_PUBLIC_PUSHER_KEY,
-        PUSHER_SECRET: process.env.PUSHER_SECRET,
-        NEXT_PUBLIC_PUSHER_CLUSTER: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
     }
 }
 
